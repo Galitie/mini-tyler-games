db28169bc0cbc2ed542d502ad2317d9420d80b15
Galit Weinfeld <galitdances@gmail.com>
2024-04-14 13:02:06 -0400

    Added back button from main menu

--- epsilon/start_menu.gd
+++ epsilon/start_menu.gd
@@ -1,7 +1,8 @@
 extends Control
 
 @onready var anim_player = $anim_player
-@onready var scene = "res://epsilon/epsilon.tscn"
+@onready var next_scene = "res://epsilon/epsilon.tscn"
+@onready var prev_scene = "res://epsilon/epsilon.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -11,4 +12,6 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_pressed("start"):
-		get_tree().change_scene_to_file(scene)
+		get_tree().change_scene_to_file(next_scene)
+	if Input.is_action_pressed("go_back"):
+		get_tree().change_scene_to_file(prev_scene)

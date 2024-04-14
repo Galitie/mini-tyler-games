87869dd6cf0b3b84b48314438104ea17f3b76077
Galit Weinfeld <galitdances@gmail.com>
2024-04-14 12:57:51 -0400

    Updated start scene so it moves to epsilon scene on pressing start

--- epsilon/start_menu.gd
+++ epsilon/start_menu.gd
@@ -1,6 +1,7 @@
 extends Control
 
 @onready var anim_player = $anim_player
+@onready var scene = "res://epsilon/epsilon.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,4 +10,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_pressed("start"):
+		get_tree().change_scene_to_file(scene)

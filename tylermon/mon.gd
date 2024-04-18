1392764e2b51f7a1bb1435b378a9930ee65e4113
Galit Weinfeld <galitdances@gmail.com>
2024-04-18 18:04:21 -0400

    Added a bunch of nonsense

--- tylermon/mon.gd
+++ tylermon/mon.gd
@@ -9,36 +9,41 @@ var strength: int = 1
 var elm_type
 var commands
 var current_state : State
-enum State {WALK_RANDOM, BASIC_ATTACK, IDLE, SPECIAL_ATTACK}
+enum State {WALK_RANDOM, BASIC_ATTACK, IDLE, SPECIAL_ATTACK, KNOCKED_OUT}
 var destination : Vector2 
 var target
 var change_mind
 @onready var anim_player = $anim_player
 @onready var timer = $timer
+@onready var hp_bar = $hp_bar
+@onready var hurt_box = $hurt_box
+@onready var basic_atk_box = $basic_atk_box
 
 func _ready():
 	current_state = State.IDLE
 	change_mind = false
+	hp_bar.max_value = health
+	hp_bar.value = health
 	timer.wait_time = 1
+	basic_atk_box.get_child(0).disabled = true
 	timer.start()
 
+
 func _physics_process(delta):
+	if health <= 0:
+		set_state(State.KNOCKED_OUT)
 	move_and_slide()
 	#if velocity.length() > 0:
-		#animation here
-		#pass
+		#walking animation here
 	update_state(current_state, delta)
-	if change_mind:
+	if change_mind and current_state != State.KNOCKED_OUT:
 		var random_number = randi_range(0, 50)
 		if random_number < 10:
 			set_state(State.WALK_RANDOM)
-			print("walk_random")
 		if random_number > 10 and random_number < 20:
 			set_state(State.BASIC_ATTACK)
-			print("basic_attack")
 		if random_number > 20 and random_number < 30:
 			set_state(State.IDLE)
-			print("idle")
 		if random_number > 30 and random_number < 40:
 			pass
 			#set_state(player.requested_state)
@@ -48,10 +53,13 @@ func _physics_process(delta):
 		$sprite.flip_h = true
 	change_mind = false
 
+		
+
 
 func set_state(state):
 	match state:
 		State.WALK_RANDOM:
+			basic_atk_box.get_child(0).disabled = true
 			destination = Vector2(randf_range(-1,1), randf_range(-1,1)).normalized()
 		State.BASIC_ATTACK:
 			velocity = Vector2()
@@ -59,7 +67,12 @@ func set_state(state):
 			#var attack = attacks.random()
 			#anim_player.play(attack.name)
 		State.IDLE:
+			basic_atk_box.get_child(0).disabled = true
 			velocity = Vector2()
+		State.KNOCKED_OUT:
+			basic_atk_box.get_child(0).disabled = true
+			velocity = Vector2()
+			$sprite.rotation = 90
 	current_state = state
 
 
@@ -68,10 +81,17 @@ func update_state(state, delta):
 		State.WALK_RANDOM:
 			velocity = destination * speed 
 		State.BASIC_ATTACK:
-			pass
+			basic_atk_box.get_child(0).disabled = false
 		State.IDLE:
 			pass
 
 
 func _on_timer_timeout():
 	change_mind = true
+
+
+func _on_hurt_box_area_entered(area):
+	if area == basic_atk_box: return
+	print("mon was hit!")
+	health -= 5
+	hp_bar.value = health

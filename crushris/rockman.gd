32744643ae58a2223804577efc6bc9713ba412eb
raamweinfeld <raamweinfeld@gmail.com>
2024-05-26 11:57:48 -0400

    Changed jump button method

--- crushris/rockman.gd
+++ crushris/rockman.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 const SPEED: float = 150.0
 const JUMP_VELOCITY: float = -450.0
 
+var jumping: bool = false
 var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 @onready var kill_zone: StaticBody2D = $kill_zone
@@ -25,10 +26,12 @@ func _physics_process(delta: float) -> void:
 	
 	if not is_on_floor():
 		velocity.y += gravity * delta
+	else:
+		jumping = false
 
-	for event in InputMap.action_get_events("jump"):
-		if event.device == controller_port && Input.is_action_just_pressed("jump"):
-			velocity.y = JUMP_VELOCITY
+	if Input.is_joy_button_pressed(controller_port, JOY_BUTTON_A) && !jumping:
+		velocity.y = JUMP_VELOCITY
+		jumping = true
 
 	var direction = Input.get_joy_axis(controller_port, JOY_AXIS_LEFT_X)
 	if direction:

2820ab2ce0ba25620860e6dd2dedb4270cc81b5a
raamweinfeld <raamweinfeld@gmail.com>
2024-08-27 17:54:27 -0400

    Added color blending between first player

--- menu/menu.gd
+++ menu/menu.gd
@@ -41,7 +41,7 @@ var thumbnail_titles: Array = [
 var thumbnail_descs: Array = [
 	"It's Tetris®, but with more pain and violence! Try to crush your friends in this absolutely original new game!\n\n2-4 Players",
 	"You like Digimon®? No?! Well, too bad! Customize your Tylermon™ and watch it fight to the death! Yeah!\n\n2-4 Players",
-	"test",
+	"Three guys love to stand out, but one of them prefers to blend in. You'll need them all to solve these puzzles!\n\n4 Players",
 	"test",
 	"Would you give your life? Not for honor, but for you? Now you can do that, but with three other people!\n\n1-4 Players"
 ]
--- spotlight/player.gd
+++ spotlight/player.gd
@@ -1,21 +1,37 @@
 extends CharacterBody2D
 
 @export var controller_port: int = 0
-@export var color: Color = Color.WHITE
 
 var move_speed: float = 200
 var jump_impulse: float = -400
 var fall_speed: float = 1200
 const MAX_FALL_SPEED: float = 1000
 
-var ping_timer: float = 0
-var ping_timer_length: float = 3
+var in_blue: bool = false
+var in_green: bool = false
+var in_red: bool = false
+
+var prev_color: Color = Color.WHITE
+var next_color: Color = Color.WHITE
+const COLOR_CHANGE_SPEED: float = 12.0
 
 func _ready() -> void:
-	$sprite.modulate = color
-	$sonar.color = color
-	#$sonar.light_mask = controller_port + 1
-	#$sonar.range_item_cull_mask = controller_port + 1
+	match controller_port:
+		0:
+			$sprite.modulate = Color.WHITE
+		1:
+			$sprite.modulate = Color.BLUE
+		2:
+			$sprite.modulate = Color.GREEN
+		3:
+			$sprite.modulate = Color.RED
+			
+	if controller_port != 0:
+		$sonar.color = $sprite.modulate
+		$sonar_area.collision_layer = controller_port + 1
+	else:
+		$body.collision_layer = 1
+		$sonar.visible = false
 
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
@@ -25,12 +41,32 @@ func _physics_process(delta: float) -> void:
 
 	if Controller.IsControllerButtonJustPressed(0, JOY_BUTTON_A) and is_on_floor():
 		velocity.y = jump_impulse
-		
-	#ping_timer += 1.0 * delta
-		
-	#if ping_timer > ping_timer_length:
-	#	ping_timer = 0
-	#	$animation_player.play("ping")
+	
+	if controller_port == 0:		
+		var spotlights = $body.get_overlapping_areas()
+		in_blue = false
+		in_green = false
+		in_red = false
+		for spotlight in spotlights:
+			match spotlight.collision_layer:
+				2:
+					in_blue = true
+				3:
+					in_green = true
+				4:
+					in_red = true
+	
+		prev_color = $sprite.modulate
+		if in_red && in_green:
+			next_color = Color.YELLOW
+		if in_red && in_blue:
+			next_color = Color.MAGENTA
+		if in_blue && in_green:
+			next_color = Color.CYAN
+		if in_red && in_green && in_blue:
+			next_color = Color.WHITE
+			
+		$sprite.modulate = lerp(prev_color, next_color, COLOR_CHANGE_SPEED * delta)
 		
 	var direction: Vector2 = Controller.GetLeftStick(0)
 	if direction:

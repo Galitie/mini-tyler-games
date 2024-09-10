ba9eff390b1e3991f55a0745d65f44d6ea2b347c
Galit Weinfeld <galitdances@gmail.com>
2024-09-09 23:23:04 -0400

    Added checkmarks for desktop settings

--- tyler98/minigames/desktop_options.gd
+++ tyler98/minigames/desktop_options.gd
@@ -13,5 +13,7 @@ func _process(_delta):
 func add_to_counter():
 	counter += 1
 	if counter == 1:
+		get_parent().owner.get_node("particles").emitting = true
+		await get_parent().owner.get_node("particles").finished
 		get_tree().get_root().get_node("main").task_completed(2)
 		item.exhausted = true

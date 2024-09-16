c680de573f5cfcfbf237e3d1245aa14f568261e6
Galit Weinfeld <galitdances@gmail.com>
2024-09-15 22:17:51 -0400

    Added tutorial to Tylermon

--- tylermon/title.gd
+++ tylermon/title.gd
@@ -2,11 +2,15 @@ extends Control
 
 @onready var anim_player = $anim_player
 @onready var game_scene = "res://tylermon/tylermon.tscn"
+@onready var tutorial = false
 
 func _ready():
 	anim_player.play("pulse")
 
 
-func _physics_process(delta):
-	if Controller.IsControllerButtonJustPressed(0, JOY_BUTTON_START):
+func _physics_process(_delta):
+	if Controller.IsControllerButtonJustPressed(0, JOY_BUTTON_START) and !tutorial:
+		%title_screen.visible = false
+		tutorial = true
+	if Controller.IsControllerButtonJustPressed(0, JOY_BUTTON_A) and tutorial:
 		get_tree().change_scene_to_file(game_scene)
